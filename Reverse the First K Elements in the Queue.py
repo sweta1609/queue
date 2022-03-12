53e5b36d4f5df7946bd29529cc12807a95e533f2
sweta <52633654+sweta1609@users.noreply.github.com>
2022-03-12 20:42:25 +0530

    Create Reverse the First K Elements in the Queue.py

---
+++ Reverse the First K Elements in the Queue.py
@@ -0,0 +1,57 @@
+from sys import stdin
+import queue
+
+def reverseKElements(inputQueue, k) :
+    #Your code goes here
+     if (inputQueue.empty() == True or
+             k > inputQueue.qsize()):
+        return
+     if (k <= 0):
+        return
+ 
+     Stack = []
+     for i in range(k):
+        Stack.append(inputQueue.queue[0])
+        inputQueue.get()
+     while (len(Stack) != 0 ):
+        inputQueue.put(Stack[-1])
+        Stack.pop()
+     for i in range(inputQueue.qsize() - k):
+        inputQueue.put(inputQueue.queue[0])
+        inputQueue.get()
+'''-------------- Utility Functions --------------'''
+
+
+#Takes a list as a stack and returns whether the stack is empty or not
+def isEmpty(stack) :
+    return len(stack) == 0
+
+
+#Takes a list as a stack and returns the element at the top
+def top(stack) :
+    #assuming the stack is never empty
+    return stack[len(stack) - 1]
+
+
+
+def takeInput():
+    n_k = list(map(int, stdin.readline().strip().split(" ")))
+    n = n_k[0]
+    k = n_k[1]
+
+    qu = queue.Queue()
+    values = list(map(int, stdin.readline().strip().split()))
+
+    for i in range(n) :
+        qu.put(values[i])
+
+    return k, qu
+
+
+#main
+k, qu = takeInput()
+
+reverseKElements(qu, k)
+
+while not qu.empty() :
+        print(qu.get(), end = " ")
